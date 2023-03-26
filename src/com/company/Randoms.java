813e88e05645295c6326f822efeda11b35fdb93d
ValeriiSch <val26-sh@mail.ru>
2023-03-26 21:10:42 +0500

    Доработка

--- src/com/company/Randoms.java
+++ src/com/company/Randoms.java
@@ -1,31 +1,38 @@
 package com.company;
 
 import java.util.Iterator;
-import java.util.PrimitiveIterator;
 import java.util.Random;
 
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-    protected PrimitiveIterator.OfInt iter;
+    protected int max, min;
 
     public Randoms(int min, int max) {
         random = new Random();
-        iter = random.ints(min, (max + 1)).iterator();
+        this.max = max;
+        this.min = min;
     }
 
     @Override
     public Iterator<Integer> iterator() {
 
-        return new Iterator<Integer>() {
+        return new Iterator<>() {
+            final int diff = max - min;
+
             @Override
             public boolean hasNext() {
-                return true;
+                if (max >= min) {
+                    return true;
+                } else {
+                    System.out.println("Максимальная граница диапазона должна быть больше минимальной");
+                    return false;
+                }
             }
 
             @Override
             public Integer next() {
-                return iter.nextInt();
+                return random.nextInt(diff + 1) + min;
             }
         };
     }
